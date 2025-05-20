d495506cb6b1e6d6936bfad28424ddff5a92b255
Joost Soeterbroek <j.soeterbroek@dictu.nl>
2025-05-20 13:03:54 +0200

    minor

--- include/globals.h
+++ include/globals.h
@@ -5,21 +5,20 @@
 #include "config.h"
 
 #define EPOCH_VERSION_MAJOR 0
-#define EPOCH_VERSION_MINOR 1
+#define EPOCH_VERSION_MINOR 2
 #define EPOCH_VERSION_PATCH 1
 
 static inline int epoch_version_major(void) {
-    return EPOCH_VERSION_MAJOR;
+  return EPOCH_VERSION_MAJOR;
 }
 static inline int epoch_version_minor(void) {
-    return EPOCH_VERSION_MINOR;
+  return EPOCH_VERSION_MINOR;
 }
 static inline int epoch_version_patch(void) {
-    return EPOCH_VERSION_PATCH;
+  return EPOCH_VERSION_PATCH;
 }
 static inline String get_timezone(void) {
-    return TIMEZONE_LOC;
+  return TIMEZONE_LOC;
 }
-
 
 #endif
